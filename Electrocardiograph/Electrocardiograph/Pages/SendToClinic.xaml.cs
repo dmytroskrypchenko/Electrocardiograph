d590da75c22ed293fe858374c424d9b512f74721
Dmytro <dmytroskrypchenko@gmail.com>
2018-05-01 20:34:32 +0300

    implement send email to clinick

--- Electrocardiograph/Electrocardiograph/Pages/SendToClinic.xaml.cs
+++ Electrocardiograph/Electrocardiograph/Pages/SendToClinic.xaml.cs
@@ -1,5 +1,6 @@
 ﻿namespace Electrocardiograph.Pages
 {
+    using Plugin.Messaging;
     using System;
     using Xamarin.Forms;
     using Xamarin.Forms.Xaml;
@@ -14,6 +15,17 @@
 
         private void OnSendToClinicButtonClicked(object sender, EventArgs e)
         {
+            var emailMessenger = CrossMessaging.Current.EmailMessenger;
+            if (emailMessenger.CanSendEmail)
+            {
+                var email = new EmailMessageBuilder()
+                    .To("dim_skrip@mail.ru")
+                    .Subject("Xamarin Messaging Plugin")
+                    .Body("Hello from your friends at Xamarin!")
+                    .Build();
+
+                emailMessenger.SendEmail(email);
+            }
         }
     }
 }
